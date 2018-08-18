c6803d45cbbc75d60c2b08f153bf87ae1471acfe
Berzel Best Tumbude <berzelbtumbude@gmail.com>
2018-08-18 09:34:51 +0200

    Added facades, service provider, and composer psr-4 autoloader

---
+++ src/PaynowServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Berzel\Paynow;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaynowServiceProvider extends ServiceProvider
+{
+    public function register ()
+    {
+        $this->app->bind('paynow', 'Berzel\Paynow\Paynow');
+
+        $config = __DIR__ . '/../config/paynow.php';
+        $this->mergeConfigFrom($config, 'paynow');
+        $this->publishes([__DIR__ . '/../config/paynow.php' => config_path('paynow.php')], 'config');
+    }
+
+    public function boot ()
+    {
+        # code...
+    }
+}
