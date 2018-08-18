7a020ab6803443abc1c596e530c03f57ba7f3cec
Berzel Best Tumbude <berzelbtumbude@gmail.com>
2018-08-18 16:03:35 +0200

    Use tag paynow-config instead of config

--- src/PaynowServiceProvider.php
+++ src/PaynowServiceProvider.php
@@ -12,7 +12,7 @@ class PaynowServiceProvider extends ServiceProvider
 
         $config = __DIR__ . '/../config/paynow.php';
         $this->mergeConfigFrom($config, 'paynow');
-        $this->publishes([__DIR__ . '/../config/paynow.php' => config_path('paynow.php')], 'config');
+        $this->publishes([__DIR__ . '/../config/paynow.php' => config_path('paynow.php')], 'paynow-config');
     }
 
     public function boot ()
